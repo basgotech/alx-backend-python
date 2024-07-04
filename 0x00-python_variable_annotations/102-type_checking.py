3627a65fc66e7e9676c834d6df0dd11b8799ef5d
Basliel Tegegn <basgolilo5@gmail.com>
2024-07-04 14:19:43 -0700

    fix last

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Zoom Arr """
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """ Zoom Array Function """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
