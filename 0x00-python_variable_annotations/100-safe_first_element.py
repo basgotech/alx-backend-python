3ad9ff9aae4d2b148261c460788e4d5a47415990
Basliel Tegegn <basgolilo5@gmail.com>
2024-07-04 14:11:54 -0700

    do

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -2,9 +2,10 @@
 """ Duck typing """
 from typing import Any, Union, Sequence, Iterable, List, Tuple
 
+
 # The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """ Safe first element """
+    """ do the element """
     if lst:
         return lst[0]
     else:
